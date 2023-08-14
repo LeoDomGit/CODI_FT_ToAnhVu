97d30329487e10b41b9965e6757d5870bdd90ad6
VuToo <minhtriet.2808@gmail.com>
2023-08-14 16:19:26 +0700

    buoi1

---
+++ buoi_1/main.js
@@ -0,0 +1,23 @@
+// alert('xin chào Vũ')
+// var name = prompt('Nhập tên của bạn')
+// alert('Tôi tên là: '+ name )
+var arr = [];
+function getName() {
+    var name = '';
+    name = document.getElementById('name').value;
+    arr.push(name);
+    const div = document.getElementsByClassName('div1');
+    console.log(arr);
+
+    var str = '';
+    arr.forEach(el => {
+        str +=el + `<br>`;
+    });
+    console.log(str);
+    div[1].innerHTML = str;
+}
+
+
+
+
+
